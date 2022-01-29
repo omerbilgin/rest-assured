ca2f9a395a95d23fc51f4fede7dab0b882626e43
Ömer Bilgin <32438592+omerbilgin@users.noreply.github.com>
2022-01-29 23:15:18 +0100

    fixed missing junit-jupiter depencency of maven-surefire-plugin junit-jupiter that was resulting tests are not executed during maven goal execution

--- src/test/java/com/bilgin/RestAssuredTest.java
+++ src/test/java/com/bilgin/RestAssuredTest.java
@@ -1,24 +1,18 @@
 package com.bilgin;
 
-
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.BeforeAll;
-
 import io.restassured.RestAssured;
-
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class RestAssuredTest {
 
-  private static String baseUri = "http://reqres.in";
-  private static  String basePath = "/api/users/2";
-
   @BeforeAll
   public static void initialize() {
-    RestAssured.baseURI = baseUri;
-    RestAssured.basePath = basePath;
+    RestAssured.baseURI = "http://reqres.in";
+    RestAssured.basePath = "/api/users/2";
   }
 
   @Test
